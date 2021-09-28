cd42ae91921bb41e6e2374874c0bd1c681c355f1
Nicole Quijada <quijadabnicolea@gmail.com>
2021-09-28 19:55:36 -0300

    agregando la data de los productos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ function Home () {
     return (
         <>
         <header className="header">
-        <a href="https://imgur.com/Qv0BVg7"><img className="Logo" src="https://i.imgur.com/Qv0BVg7.png" title="source: imgur.com" /></a>
+        <a href="https://imgur.com/Qv0BVg7"><img className="Logo" alt="logo" src="https://i.imgur.com/Qv0BVg7.png" title="source: imgur.com" /></a>
         </header>
         <body className="body">
         <Link className="btn btn-warning" role="button" to="/Menu"> {" "} Mesero </Link>
