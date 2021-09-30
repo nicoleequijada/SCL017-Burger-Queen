3755babff28161fe418bf895b6357dde18744300
Nicole Quijada <quijadabnicolea@gmail.com>
2021-09-30 19:42:49 -0300

    creando componente para total a pagar

--- src/pages/MenuOption.jsx
+++ src/pages/MenuOption.jsx
@@ -3,13 +3,15 @@ import Button from "@restart/ui/esm/Button"
 function MenuOption (props) {
     return (
         
-        <div key={props.data.id}>
+        <div className="card-menu" key={props.data.id}>
         <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={props.data.img} alt="coffe"/>
         <Card.Body>
-        <Card.Title>{props.data.name}</Card.Title>
-        <Card.Text>Precio {props.data.price}$</Card.Text>
-        <Button variant="primary">Agregar</Button>
+        <Card.Title className="card-title">{props.data.name}</Card.Title>
+        <Card.Text className="card-price">Precio {props.data.price}$</Card.Text>
+        <div className="d-grid gap-2">
+        <Button className="button-add" variant="primary" size="lg">Agregar</Button>
+        </div>
         </Card.Body>
         </Card>
         </div>
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -3,28 +3,48 @@ import Button from "@restart/ui/esm/Button";
 import Data from "./menu.json";
 import MenuOption from  "./MenuOption.jsx"
 import Header from "./Header";
-
+import TotalSummary from "./TotalSummary";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function Menu () {
     console.log(Data.Data)
     const MenuOptions = Data.Data.map((DataActual) => {
         return (
-           <MenuOption data={DataActual}/>
-        )
-
-    })
-    return(
-        <>
-        <Header show={true}/>
-        <div className="body">
+            <MenuOption data={DataActual}/>
+            )
             
-        <h1>Prueba de menu</h1>
-        <div className="menu">
-        {MenuOptions}
-        </div>
-        <Button  className="cliente" variant="warning">Nombre del cliente</Button>{' '}
-        </div>
-        </>
-    )        
-}
-export default Menu
+        })
+        return(
+            <>
+            <Header show={true}/>
+            <div className="body">
+            <div>
+            <Button  className="cliente" variant="warning">Nombre del cliente</Button>
+            <input className="cliente-input" placeholder="nombre cliente"></input>
+            </div>
+            <div>
+            <Button  className="cliente" variant="warning">Numero de mesa</Button>
+            <input className="cliente-input" placeholder="numero de mesa"></input>
+            </div>
+            
+            
+            <div className="menu">
+           
+            <Row>
+            <Col className="menu-col" sm={9}>
+            {MenuOptions}
+            </Col>
+            
+            <Col sm={2}>
+            <TotalSummary />
+            </Col>
+            </Row>
+            
+            
+            </div>
+            </div>
+            </>
+            )        
+        }
+        export default Menu
