3101f89549dd6f2237262cf96e63c55d6e910af0
Nicole Quijada <quijadabnicolea@gmail.com>
2021-10-12 17:56:25 -0300

    modificando css

--- src/components/OrderProduct/OrderProduct.jsx
+++ src/components/OrderProduct/OrderProduct.jsx
@@ -3,21 +3,21 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { ListGroup } from 'react-bootstrap'
+import '../OrderProduct/orderProduct.css'
 
 function OrderProduct (props) {
 	const {product, index} = props
 
 	return (
 		<Row>
-		  	<Card>
-		  	  <Card.Header>{product.name}</Card.Header>
-			  <Card.Body>	
-			    <Card.Text>
-			     {product.price}
-			    </Card.Text>
-			    <Button variant="primary" onClick={()=>{props.removeOrderProduct(index)}}>Delete</Button>
-			  </Card.Body>
+			<Card className="card-order">
+ 				<ListGroup variant="flush">
+    				<ListGroup.Item>{product.name} ${product.price} </ListGroup.Item>
+				<Button className="button-delete" variant="danger" onClick={()=>{props.removeOrderProduct(index)}}>Delete</Button>
+  				</ListGroup>
 			</Card>
+		  	
 		</Row>
 	)
 
