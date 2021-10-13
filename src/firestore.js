b5c137b5c1cf37b5108005013ade3c30a9a3d79c
Nicole Quijada <quijadabnicolea@gmail.com>
2021-10-13 08:38:22 -0300

    exportando correctamente saveOrder

--- src/firestore.js
+++ src/firestore.js
@@ -5,11 +5,11 @@ import { collection, addDoc } from "firebase/firestore";
 
 const db = getFirestore(app);
 
-async function saveData (data) {
-    const docRef = await addDoc(collection(db, "pedidos"), data);
+async function saveOrder (OrderDetail) {
+    const docRef = await addDoc(collection(db, "pedidos"), OrderDetail);
     console.log("Document written with ID: ", docRef.id);
 }
 
 
 
-export default saveData
+export default saveOrder
